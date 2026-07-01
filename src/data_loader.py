dce1c494d3d4eaa098dabb30f001d4ee2511a729
oanhnguyen0529 <oanhnguyen0529@gmail.com>
2026-07-01 10:29:39 +0700

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,16 @@
+import pandas as pd
 
+def load_raw_data(path="data/raw/bank-additional-full.csv"):
+    df = pd.read_csv(path, sep=';')
+    return df
+
+def get_feature_info():
+    return {
+        "numeric": ["age","duration","campaign","pdays","previous",
+                    "emp.var.rate","cons.price.idx","cons.conf.idx",
+                    "euribor3m","nr.employed"],
+        "categorical": ["job","marital","education","default",
+                        "housing","loan","contact","month",
+                        "day_of_week","poutcome"],
+        "target": "y"
+    }
